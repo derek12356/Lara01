c448e937b839d0ff75cd1f771dc8550e949d9909
derek wu <wjn123562077@gmail.com>
2019-06-12 20:45:04 +0000

    style init

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-<h1>Home</h1>
-@endsection
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      This is  <a href="https://learnku.com/courses/laravel-essential-training">Laravel</a> home page
+    </p>
+    <p>
+      Starts from here
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">Register Now</a>
+    </p>
+  </div>
+@stop
