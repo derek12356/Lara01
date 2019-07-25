48fb77884d3ffe07bea5a63d6869284fe03ef701
derek-5 <wjn123562077@gmail.com>
2019-07-25 17:17:36 -0400

    hide private users' post

--- resources/views/posts/_post.blade.php
+++ resources/views/posts/_post.blade.php
@@ -1,4 +1,5 @@
 <li class="media mt-4 mb-4">
+  @can('view', $user)
   <a href="{{ route('users.show', $user->id )}}">
     <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
   </a>
@@ -6,7 +7,7 @@
     <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $post->created_at->diffForHumans() }}</small></h5>
     {{ $post->content }}
   </div>
-
+  @endcan
   @can('destroy', $post)
   <form action="{{ route('posts.destroy', $post->id) }}" method="POST" onsubmit="return confirm('Are you sure to delete.');">
   	@csrf
